89438f1e0f7b810cdbe0f77b7e25764749f0fd8f
Dan Stewart <git@mail.danstewart.dev>
2023-07-09 15:11:02 +0100

    WIP: Add render_block for frames

--- app/views/holidays.py
+++ app/views/holidays.py
@@ -11,6 +11,22 @@ logger = get_logger(__name__)
 @v.get("/holidays")
 @login_required
 def holiday_summary():
+    from flask import request
+
+    from app.lib.blocks import render_block
+
+    # If request is coming from a DynamicFrame then only render the `content` block
+    # TODO:
+    # - Turn this into a decorator
+    # - Send the header from dynamic frames
+    if request.headers.get("X-DynamicFrame"):
+        return render_block(
+            "pages/holidays/upcoming.html.j2",
+            "content",
+            next_public_holiday=time.get_next_public_holiday(),
+            page="upcoming",
+        )
+
     return render_template(
         "pages/holidays/upcoming.html.j2",
         page="upcoming",
