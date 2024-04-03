baea993d1fdc299a1bce357bec9d3311ed3af98a
Jonas <j.hammerskog@gmail.com>
2024-04-03 12:25:53 +0100

    Fix

--- app/lib/util/ensure.py
+++
@@ -1,9 +0,0 @@
-def ensure_list(value):
-    """
-    Takes a value, if it's a list return it
-    Otherwise make it a list
-    """
-    if isinstance(value, list):
-        return value
-    else:
-        return [value]
--- app/static/js/vendor/binder/binder.js
+++
@@ -1,5 +0,0 @@
-import { registerControllers } from "./register.js";
-import { Controller } from "./controller.js";
-import { html, css } from "./util.js";
-
-export { registerControllers, Controller, html, css };
