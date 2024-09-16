29ba06c79f634fb0cebda33cf3426e96cd8bbb6a
Dan Stewart <git@mail.danstewart.dev>
2024-09-16 20:39:07 +0100

    Finish slack auth

--- app/views/callback.py
+++ app/views/callback.py
@@ -29,7 +29,7 @@ def slack_oauth_callback():
             "client_id": app.config["SLACK_CLIENT_ID"],
             "client_secret": app.config["SLACK_CLIENT_SECRET"],
             "code": code,
-            "redirect_uri": "",  # TODO
+            "redirect_uri": f"{app.config['HOST']}/callback/slack",
         },
         headers={
             "Content-Type": "application/x-www-form-urlencoded",
--- app/views/settings.py
+++ app/views/settings.py
@@ -1,11 +1,13 @@
 from decimal import Decimal
 
+import sqlalchemy as sa
 from flask import Blueprint, request
 
 from app.controllers import settings
 from app.controllers.user.util import admin_only, login_required
 from app.lib.blocks import frame, render
 from app.lib.logger import get_logger
+from app.models import UserToSlackToken
 
 v = Blueprint("settings", __name__)
 logger = get_logger(__name__)
@@ -103,6 +105,7 @@ def account_settings():
 def slack_settings():
     from flask import current_app as app
 
+    from app import db
     from app.controllers.user.util import get_user
 
     user = get_user()
@@ -118,29 +121,19 @@ def slack_settings():
     if request.form:
         from flask import flash, redirect
 
-        if request.form.get("validate"):
-            from app.lib import validate as v
+        action = request.form.get("action")
+        if action == "disconnect":
+            db.session.execute(sa.delete(UserToSlackToken).where(UserToSlackToken.user_id == user.id))
+            db.session.commit()
 
-            validation = v.validate_form(
-                values=dict(request.form),
-                checks={
-                    # TODO: Run this through arrow or pytz to validate
-                    "timezone": v.Check(regex=r"\w+\/\w+"),
-                    "holiday_location": v.Check(options=["GB/ENG", "GB/NIR", "GB/WLS", "GB/SCT"]),
-                    "week_start": v.Check(options=["0", "1", "2", "3", "4", "5", "6"]),
-                    "hours_per_day": v.Check(
-                        func=lambda x: Decimal(x) > 0,
-                        message="Must be a positive number",
-                    ),
-                },
-            )
-            return validation.errors or validation.success
+            flash("Your slack account has been disconnected", "success")
+            return redirect("/settings/slack")
 
         settings.update(**request.form)
         flash("Settings saved", "success")
         return redirect("/dash")
 
-    return render("pages/settings.html.j2", settings=settings.fetch(), page="general")
+    return render("pages/settings.html.j2", page="slack_config")
 
 
 @v.route("/settings/admin", methods=["GET", "POST"])
